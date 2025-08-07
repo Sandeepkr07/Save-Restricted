ea0e0a91bd4d51ef61737dd172eab71c8e2cafe8
Sandeep Kashyap 😻 <155357259+Sandeepkr07@users.noreply.github.com>
2025-08-08 01:50:00 +0530

    Update shared_client.py

--- shared_client.py
+++ shared_client.py
@@ -4,37 +4,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 INST_COOKIES = """
-# put insta cookies here if needed
+# write your Instagram cookies here if needed
 """
 
 YTUB_COOKIES = """
-# put yt cookies here if needed
+# write your YouTube cookies here if needed
 """
 
-API_ID = "24558120"
-API_HASH = "85bd1a317c44733e6620ec82a7053fe9"
-BOT_TOKEN = "7264260495:AAHXaiFmOoJ0KnymkeqaDTy0UpsJw1HINY8"
-MONGO_DB = "mongodb+srv://sandeepkrbth64:NbPbc81D9Z7CXhLj@sandeepkr.5zk8q.mongodb.net/?retryWrites=true&w=majority&appName=Sandeepkr"
-OWNER_ID = [6005294654]
-DB_NAME = "telegram_downloader"
-STRING = None
-LOG_GROUP = -1002344616377
-FORCE_SUB = -1002191173069
-MASTER_KEY = "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq"
-IV_KEY = "s7Yx5CpVmE3F"
-YT_COOKIES = YTUB_COOKIES
-INSTA_COOKIES = INST_COOKIES
-FREEMIUM_LIMIT = 0
-PREMIUM_LIMIT = 500
-JOIN_LINK = "https://t.me/team_spy_pro"
-ADMIN_CONTACT = "https://t.me/username_of_admin"
-
-from pyrogram import Client
-
-app = Client("bot", api_id=int(API_ID), api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-async def start_client():
-    await app.start()
-    print("Bot Started.")
-
-
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split()))
+MONGO_DB = os.getenv("MONGO_DB")
+DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
+STRING = os.getenv("STRING", None)
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002344616377"))
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002191173069"))
+MASTER_KEY = os.getenv("MASTER_KEY")
+IV_KEY = os.getenv("IV_KEY")
+YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
+INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
+JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro")
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/its_sandeepkashyap")
